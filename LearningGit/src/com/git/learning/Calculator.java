2b5bde884f5713ec834610fdf252a7624573054a
Gasser <gasser.salah.m@gmail.com>
2025-04-12 05:05:47 -0400

    edited add method, added new multiply method

--- LearningGit/src/com/git/learning/Calculator.java
+++ LearningGit/src/com/git/learning/Calculator.java
@@ -1,5 +1,22 @@
 package com.git.learning;
 
 public class Calculator {
+	
+	
+	
+	public int add(int x, int y) {
+		int result = 0;
+		result = x + y;
+		return result;
+	}
 
+	
+	public int multiply(int x, int y) {
+		return x* y;
+	}
+	
+	
 }
+
+
+
